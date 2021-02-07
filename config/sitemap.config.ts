b32f3cb5bc8d8e8cdaa02a59636db5ee9a4eb19e
Davide Giovanni Steccanella <33760151+davidegiovanni@users.noreply.github.com>
2021-02-07 23:53:57 +0100

    feat: update sitemap

--- config/sitemap.config.ts
+++ config/sitemap.config.ts
@@ -1,6 +1,7 @@
 export default {
     hostname: process.env.BASE_URL || 'https://davidegiovanni.com',
     routes: [
-        '/it/blog/immagini-di-brand-forti-ed-efficaci'
+        '/it/blog/immagini-di-brand-forti-ed-efficaci',
+        '/it/blog/i-designer-usano-manipolazioni-psicologiche-di-continuo-quali-sono-gli-effetti-sul-cervello'
     ]
 }

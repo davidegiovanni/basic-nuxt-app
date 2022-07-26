1bbf8992e9aa55e160da71ae0dd21c9f4b7808c8
Davide Giovanni Steccanella <33760151+davidegiovanni@users.noreply.github.com>
2022-07-27 01:29:06 +0200

    chore: remove old files

--- config/sitemap.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    hostname: process.env.BASE_URL || 'https://davidegiovanni.com',
-    routes: [
-        '/it/blog/immagini-di-brand-forti-ed-efficaci',
-        '/it/blog/i-designer-usano-manipolazioni-psicologiche-di-continuo-quali-sono-gli-effetti-sul-cervello'
-    ]
-}
--- types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.yaml' {
-    const content: any;
-    export default content;
-}
-
-declare module 'nuxt-gsap-module';

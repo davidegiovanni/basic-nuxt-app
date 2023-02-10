d1d5692408ea17de7638e66cefcef056637c11f6
Davide Giovanni Steccanella <33760151+davidegiovanni@users.noreply.github.com>
2023-02-10 01:32:57 +0100

    fix: sitemap

--- app/routes/robots[.]txt.tsx
+++ app/routes/robots[.]txt.tsx
@@ -3,13 +3,12 @@ import { LoaderFunction } from "@remix-run/node";
 export const loader: LoaderFunction = ({request}) => {
   const url = new URL(request.url)
   const origin = url.origin
-        const robotText = `
+  const robotText = 
+`User-agent: *
+Allow: /
 
-        User-agent: *
-        Allow: /
-    
-        Sitemap: ${origin}/sitemap.xml
-        `
+Sitemap: ${origin}/sitemap.xml
+`
       // return the text content, a status 200 success response, and set the content type to text/plain 
         return new Response(robotText,{
           status: 200,
